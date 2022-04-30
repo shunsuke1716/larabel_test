07d1676f705d98579e165803eb78bc4b7b853136
shunsuke1716 <shunsuke.fujita1014@outlook.com>
2022-04-30 12:39:17 +0900

    問題２の解答を追加

---
+++ src/app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index($text="建物です。")
+    {
+        return "$text";
+    }
+
+    public function room_num($room)
+    {
+        return "部屋番号は".$room."です";
+    }
+
+}
